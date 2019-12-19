ce549b27dab9be2f84f55cc890d6be5fc0565b4a
Yauhen <stan01@tut.by>
2019-12-19 11:09:10 +0300

    UserDao batch insert added

--- persist/src/main/java/ru/javaops/masterjava/persist/dao/UserDao.java
+++ persist/src/main/java/ru/javaops/masterjava/persist/dao/UserDao.java
@@ -2,13 +2,21 @@ package ru.javaops.masterjava.persist.dao;
 
 import com.bertoncelj.jdbi.entitymapper.EntityMapperFactory;
 import org.skife.jdbi.v2.sqlobject.*;
+import org.skife.jdbi.v2.sqlobject.customizers.BatchChunkSize;
 import org.skife.jdbi.v2.sqlobject.customizers.RegisterMapperFactory;
 import ru.javaops.masterjava.persist.model.User;
+import ru.javaops.masterjava.persist.util.AnnotationUtil;
 
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
 import java.util.List;
 
 @RegisterMapperFactory(EntityMapperFactory.class)
 public abstract class UserDao implements AbstractDao {
+    public UserDao() {
+        super();
+    }
 
     public User insert(User user) {
         if (user.isNew()) {
@@ -20,9 +28,35 @@ public abstract class UserDao implements AbstractDao {
         return user;
     }
 
+    public Iterable<User> insert(Iterable<User> users) {
+        List<User> usersWithId = new ArrayList<>();
+        List<User> usersNoId = new ArrayList<>();
+
+        for (User user : users) {
+            if (user.isNew()) {
+                usersNoId.add(user);
+            } else {
+                usersWithId.add(user);
+            }
+        }
+
+        insertWitIdBatch(usersWithId);
+        insertGeneratedIdBatch(usersNoId);
+
+        return users;
+    }
+
+    @SqlBatch("insert into users (full_name, email, flag) values (:fullName, :email, CAST(:flag AS user_flag)) ")
+    @BatchChunkSize(1000)
+    abstract int[] insertGeneratedIdBatch(@BindBean Iterable<User> user);
+
     @SqlUpdate("INSERT INTO users (full_name, email, flag) VALUES (:fullName, :email, CAST(:flag AS user_flag)) ")
     @GetGeneratedKeys
     abstract int insertGeneratedId(@BindBean User user);
+
+    @SqlBatch("INSERT INTO users (id, full_name, email, flag) VALUES (:id, :fullName, :email, CAST(:flag AS user_flag)) ")
+    @BatchChunkSize(1000)
+    abstract int[] insertWitIdBatch(@BindBean Iterable<User> user);
 
     @SqlUpdate("INSERT INTO users (id, full_name, email, flag) VALUES (:id, :fullName, :email, CAST(:flag AS user_flag)) ")
     abstract void insertWitId(@BindBean User user);
@@ -34,4 +68,16 @@ public abstract class UserDao implements AbstractDao {
     @SqlUpdate("TRUNCATE users")
     @Override
     public abstract void clean();
+
+    public  static <T extends Class<?>> void setBatchChunkSize(T obj, int size) {
+        for (Method userDaoMethod : obj.getDeclaredMethods()) {
+            if (userDaoMethod.getName().endsWith("Batch")) {
+                userDaoMethod.setAccessible(true);
+                Annotation annotation = userDaoMethod.getAnnotation(BatchChunkSize.class);
+                if (annotation != null) {
+                    AnnotationUtil.changeAnnotationValue(annotation, "value", size);
+                }
+            }
+        }
+    }
 }
