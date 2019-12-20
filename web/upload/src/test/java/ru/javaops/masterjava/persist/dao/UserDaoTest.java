94d6a1baff565a984e6e9e32aec2c2008395b28d
Yauhen <stan01@tut.by>
2019-12-20 20:56:45 +0300

    insert batch with conflict test

--- web/upload/src/test/java/ru/javaops/masterjava/persist/dao/UserDaoTest.java
+++ web/upload/src/test/java/ru/javaops/masterjava/persist/dao/UserDaoTest.java
@@ -8,9 +8,6 @@ import ru.javaops.masterjava.persist.model.User;
 
 import java.util.List;
 
-import static ru.javaops.masterjava.persist.UserTestData.FIRST6_USERS;
-import static ru.javaops.masterjava.persist.UserTestData.SECOND6_USERS;
-
 public class UserDaoTest extends AbstractDaoTest<UserDao> {
 
     public UserDaoTest() {
@@ -26,17 +23,20 @@ public class UserDaoTest extends AbstractDaoTest<UserDao> {
     public void getWithLimit() {
         UserTestData.setUp();
         List<User> users = dao.getWithLimit(5);
-        Assert.assertEquals(FIRST6_USERS, users);
+        Assert.assertEquals(UserTestData.FIRST6_USERS, users);
     }
 
     @Test
-    public void getWithLimitBatch() {
+    public void insertBatchWithConflict() {
         UserTestData.setUpBatch();
         List<User> users = dao.getWithLimit(5);
         users.forEach(u -> u.setId(null));
-        Assert.assertEquals(FIRST6_USERS, users);
-        List<User>newUsers = (List<User>) dao.insert(SECOND6_USERS);
-        List<User>conflicted = UserDao.subtractUsersByEmail(SECOND6_USERS, newUsers);
-        System.out.println(conflicted);
+        Assert.assertEquals(UserTestData.FIRST6_USERS, users);
+
+        List<User>newUsers = UserTestData.insertBatch(UserTestData.SECOND6_USERS);
+        List<User>conflicted = UserDao.subtractUsersByEmail(UserTestData.SECOND6_USERS, newUsers);
+
+        Assert.assertTrue(conflicted.contains(UserTestData.USER1));
+        Assert.assertEquals(1, conflicted.size());
     }
 }
