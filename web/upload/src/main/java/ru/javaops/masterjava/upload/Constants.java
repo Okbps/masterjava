e6c28d81db092173cdd99eeda10682d1507958ba
Yauhen <stan01@tut.by>
2019-12-23 09:57:31 +0300

    constants class added

---
+++ web/upload/src/main/java/ru/javaops/masterjava/upload/Constants.java
@@ -0,0 +1,8 @@
+package ru.javaops.masterjava.upload;
+
+public final class Constants {
+    private Constants() {}
+
+    public final static int USERS_DISPLAY_LIMIT = 20;
+    public final static int N_THREADS = 4;
+}
