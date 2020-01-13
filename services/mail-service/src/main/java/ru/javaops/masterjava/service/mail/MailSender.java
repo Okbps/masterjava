74b5616b525f9d43a348f9b2fd18d5e4b8760d94
Evgeni Pakrouski <evgeni.pakrouski@standardbank.co.za>
2020-01-13 16:46:07 +0300

    basic mail sender implementation

--- services/mail-service/src/main/java/ru/javaops/masterjava/service/mail/MailSender.java
+++ services/mail-service/src/main/java/ru/javaops/masterjava/service/mail/MailSender.java
@@ -1,12 +1,42 @@
 package ru.javaops.masterjava.service.mail;
 
+import com.typesafe.config.Config;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.mail.DefaultAuthenticator;
+import org.apache.commons.mail.Email;
+import org.apache.commons.mail.EmailException;
+import org.apache.commons.mail.SimpleEmail;
+import ru.javaops.masterjava.config.Configs;
 
 import java.util.List;
 
 @Slf4j
 public class MailSender {
+    private final static Config db = Configs.getConfig("mail.conf","yandex");
+
     static void sendMail(List<Addressee> to, List<Addressee> cc, String subject, String body) {
-        log.info("Send mail to \'" + to + "\' cc \'" + cc + "\' subject \'" + subject + (log.isDebugEnabled() ? "\nbody=" + body : ""));
+        String mailDescription = "to '" + to + "' cc '" + cc + "' subject '" + subject + (log.isDebugEnabled() ? "\nbody=" + body : "");
+
+        Email email = new SimpleEmail();
+        email.setHostName(db.getString("host"));
+        email.setSmtpPort(db.getInt("port"));
+        email.setAuthenticator(new DefaultAuthenticator(db.getString("username"), db.getString("password")));
+        email.setSSLOnConnect(true);
+        email.setSubject(subject);
+
+        try {
+            for(Addressee addressee: to){
+                email.addTo(addressee.getEmail());
+            }
+            email.setFrom(db.getString("fromName"));
+            email.setMsg(body);
+            email.send();
+            log.info("Sent mail " + mailDescription);
+        } catch (EmailException e) {
+            log.error("Failed to send mail " + mailDescription);
+            log.error(e.getMessage());
+        }
+
+
     }
 }
