994df9b15568479bf35b7eb576523a18e3a2125d
Evgeni Pakrouski <evgeni.pakrouski@standardbank.co.za>
2020-01-14 15:41:03 +0300

    addressees to string utility method added

--- services/mail-api/src/main/java/ru/javaops/masterjava/service/mail/Addressee.java
+++ services/mail-api/src/main/java/ru/javaops/masterjava/service/mail/Addressee.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.Collection;
+
 /**
  * gkislin
  * 15.11.2016
@@ -14,4 +16,8 @@ import lombok.NoArgsConstructor;
 public class Addressee {
     private String email;
     private String name;
+
+    public static String addresseesToSting(Collection<Addressee> addressees){
+        return addressees.stream().map(Addressee::getEmail).reduce((s1, s2) -> s1+","+s2).orElse("");
+    }
 }
