da86bbbc7232be0b6022f3e1ad4e20d75f2f8175
Yauhen <stan01@tut.by>
2020-01-25 12:22:56 +0300

    sql changelog updated

--- sql/databaseChangeLog.sql
+++ sql/databaseChangeLog.sql
@@ -1,43 +1,50 @@
 --liquibase formatted sql
 
 --changeset gkislin:1
-CREATE SEQUENCE common_seq START 100000;
+CREATE SEQUENCE IF NOT EXISTS common_seq START 100000;
 
-CREATE SEQUENCE mail_seq START 100000;
+CREATE SEQUENCE IF NOT EXISTS mail_seq START 100000;
 
-CREATE TABLE city (
+DO $$
+	BEGIN
+		IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'group_type') THEN
+			CREATE TYPE group_type AS ENUM ('REGISTERING', 'CURRENT', 'FINISHED');
+		END IF;
+		IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'mail_result') THEN
+			CREATE TYPE mail_result AS ENUM ('SUCCESS', 'FAILED', 'EXCEPTION');
+		END IF;
+	END;
+$$;
+
+CREATE TABLE IF NOT EXISTS city (
   ref  TEXT PRIMARY KEY,
   name TEXT NOT NULL
 );
 
 ALTER TABLE users
-  ADD COLUMN city_ref TEXT REFERENCES city (ref) ON UPDATE CASCADE;
+  ADD COLUMN IF NOT EXISTS city_ref TEXT REFERENCES city (ref) ON UPDATE CASCADE;
 
 --changeset gkislin:2
-CREATE TABLE project (
+CREATE TABLE IF NOT EXISTS project (
   id          INTEGER PRIMARY KEY DEFAULT nextval('common_seq'),
   name        TEXT UNIQUE NOT NULL,
   description TEXT
 );
 
-CREATE TYPE GROUP_TYPE AS ENUM ('REGISTERING', 'CURRENT', 'FINISHED');
-
-CREATE TABLE groups (
+CREATE TABLE IF NOT EXISTS groups (
   id         INTEGER PRIMARY KEY DEFAULT nextval('common_seq'),
   name       TEXT UNIQUE NOT NULL,
   type       GROUP_TYPE  NOT NULL,
   project_id INTEGER     NOT NULL REFERENCES project (id)
 );
 
-CREATE TABLE user_group (
+CREATE TABLE IF NOT EXISTS user_group (
   user_id  INTEGER NOT NULL REFERENCES users (id) ON DELETE CASCADE,
   group_id INTEGER NOT NULL REFERENCES groups (id),
   CONSTRAINT users_group_idx UNIQUE (user_id, group_id)
 );
 
-CREATE TYPE MAIL_RESULT AS ENUM ('SUCCESS', 'FAILED', 'EXCEPTION');
-
-CREATE TABLE mail_descriptor (
+CREATE TABLE IF NOT EXISTS mail_descriptor (
   id         INTEGER PRIMARY KEY DEFAULT nextval('mail_seq'),
   subject TEXT NOT NULL,
   to_addresses TEXT NOT NULL,
